69549caceb055822cdf6b41c7afb193d9864b8d1
sfragkoul <sfragkoul@certh.gr>
2026-02-14 09:44:09 +0200

    intro add device

--- 01_introduction.py
+++ 01_introduction.py
@@ -2,7 +2,13 @@ import diffusers
 import huggingface_hub
 import transformers
 
+from genaibook.core import get_device
+device = get_device()
+print(f"Using device: {device}")
+
 
 diffusers.logging.set_verbosity_error()
 huggingface_hub.logging.set_verbosity_error()
 transformers.logging.set_verbosity_error()
+
+
