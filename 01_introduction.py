d79eb1e824b323f78604b5cadc0116ab39d7966a
sfragkoul <sfragkoul@certh.gr>
2026-02-16 14:22:02 +0200

    gen img and txt

--- 01_introduction.py
+++ 01_introduction.py
@@ -2,13 +2,33 @@ import diffusers
 import huggingface_hub
 import transformers
 
+# check device-------------------------------------------------------------------------------
 from genaibook.core import get_device
 device = get_device()
 print(f"Using device: {device}")
 
+# Generating Images-------------------------------------------------------------------------
+import torch
+from diffusers import StableDiffusionPipeline
 
-diffusers.logging.set_verbosity_error()
-huggingface_hub.logging.set_verbosity_error()
-transformers.logging.set_verbosity_error()
+pipe = StableDiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5",
+#torch_dtype=torch.float16,
+variant="fp16",
+).to(device)
+
+prompt = "a photograph of an astronaut riding a horse"
+pipe(prompt).images[0]
+
+# Generating Text---------------------------------------------------------------------------
+from transformers import pipeline
+
+classifier = pipeline("text-classification", device=device)
+classifier("This movie is disgustingly good!")
 
 
+from transformers import set_seed
+# Setting the seed ensures we get the same results every time we run this code
+set_seed(10)
+generator = pipeline("text-generation", device=device)
+prompt = "It was a dark and stormy"
+generator(prompt)[0]["generated_text"]
